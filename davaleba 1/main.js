2cc08cde0dd0271870f4dfb26468092bf2031830
mumladze1 <141115595+mumladze1@users.noreply.github.com>
2024-07-17 13:31:33 +0400

    add

---
+++ davaleba 1/main.js
@@ -0,0 +1,21 @@
+console.log("welcome")
+
+function compare(a, b) {
+    if (a === b) {
+        return "tolia";
+    } else {
+        return "ar aris toli";
+    }
+}
+
+
+
+
+
+
+let result1 = compare(2, 7);
+console.log(result1); 
+
+
+let result2 = compare(5, 5);
+console.log(result2); 
